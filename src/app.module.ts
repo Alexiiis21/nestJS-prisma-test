8a815c98aac7116cb46fd9d8b8e6a946a0e4b51d
Alexiiis21 <acardenas22@alumnos.uaq.mx>
2024-11-22 13:21:42 -0600

    prisma.module error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './Prisma/prisma.module';
 
 @Module({
   imports: [TaskModule, PrismaModule],
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -9,7 +9,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { Task } from '.prisma/client';
+import { Task } from '@prisma/client';
 
 @Controller('task')
 export class TaskController {
